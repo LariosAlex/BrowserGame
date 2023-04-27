e8a58f190e34da7adce835c9fed21d5a3f3835dd
UnaiRiccoMoyano <un4i37@gmail.com>
2023-04-27 18:01:49 +0200

    Primer commit BDD

--- browserGame/admin.py
+++ browserGame/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Season)
+admin.site.register(Character)
+admin.site.register(Action)
+admin.site.register(ActionLog)
+admin.site.register(Log)
