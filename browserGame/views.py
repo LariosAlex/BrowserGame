4328d52facb5354209a965cc73d765a6d76bc550
Alex Larios <alexlarios49@gmail.com>
2023-04-28 18:02:37 +0200

    Landing page log/no log

--- browserGame/views.py
+++ browserGame/views.py
@@ -4,23 +4,28 @@ from .models import *
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.core.exceptions import *
+from django.utils import timezone
 def vue(request):
     context = {}
 
     return render(request, 'browserGame/index.html', context)
 
 def landing(request):
-    active_season = Season.objects.get(id=11)
+    active_season = Season.objects.get(id=1)
+    actions = None
+    characters = Character.objects.filter(season=active_season).order_by('-level', '-exp')
     if request.user.is_authenticated:
         try:
             character = Character.objects.get(season=active_season, user=request.user)
+            actions = ActionLog.objects.filter(performer=character)
         except ObjectDoesNotExist:
             character = None
     else:
         character = None
-    print(Character)
     context = {
         'season': active_season,
-        'character' : character
+        'character' : character,
+        'actions' : actions,
+        'characters': characters
     }
     return render(request, 'browserGame/landing.html', context)
