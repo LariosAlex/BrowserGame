20ba121196c7824c6ba0d22e34c0611fbf17589c
«PolTellJove» <poltelljove@gmail.com>
2023-05-02 17:59:23 +0200

    cron url done

--- browserGame/urls.py
+++ browserGame/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import activate_cron
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('activate_cron/', activate_cron, name='activate_cron'),
 ]
--- browserGame/views.py
+++ browserGame/views.py
@@ -3,7 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from .models import Character
+
 def index(request):
     context = {}
 
+    #posar aqui la funcio activate_cron?
+
     return render(request, 'browserGame/index.html', context)
+
+def activate_cron(request):
+
+    for i in Character.objects.all():
+        if i.life > 0:
+            if i.level*10 - i.mana > i.level:
+                i.mana += i.level
+            else:
+                i.mana = i.level*10
+            i.save()
+    
+    #data_cron = Seasons.objects.
+
+    return HttpResponse('Cron activated!')
+
+
+'''
+# Obtén el registro que deseas actualizar por su ID
+personaje = Personaje.objects.get(id=1)
+
+# Actualiza el valor del atributo deseado
+personaje.nombre = 'Nuevo nombre'
+
+# Guarda los cambios en la base de datos
+personaje.save()
+'''
