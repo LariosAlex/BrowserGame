27caf7d3756fbba870e050056025b1390339fc46
PolTellJove <poltelljove@gmail.com>
2023-05-11 00:39:04 +0200

    animations added

---
+++ browserGame/static/js/script.js
@@ -0,0 +1,250 @@
+button1.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    escupinada.style.display = "block";
+    escupinada.classList.add("a_escupinada");
+    cowboy.style.display = "block";
+    cowboy.classList.add("a_cowboy1");
+    setTimeout(rev_b1, 2300);
+});
+
+function rev_b1() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    escupinada.style.display = "none";
+    escupinada.classList.remove("a_escupinada");
+    cowboy.style.display = "none";
+    cowboy.classList.remove("a_cowboy1");
+};
+
+button2.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    bota.style.display = "block";
+    bota.classList.add("a_bota");
+    cowboy.style.display = "block";
+    cowboy.classList.add("a_cowboy2");
+    sang.classList.add("a_sang2");
+    setTimeout(rev_b2, 1800);
+});
+
+function rev_b2() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    bota.style.display = "none";
+    bota.classList.remove("a_bota");
+    cowboy.style.display = "none";
+    cowboy.classList.remove("a_cowboy2");
+    sang.classList.remove("a_sang2");
+};
+
+button3.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    revolver.style.display = "block";
+    revolver.classList.add("a_revolver");
+    cowboy.style.display = "block";
+    cowboy.classList.add("a_cowboy3");
+    sang.classList.add("a_sang3");
+    setTimeout(rev_b3, 2100);
+});
+
+function rev_b3() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    revolver.style.display = "none";
+    revolver.classList.remove("a_revolver");
+    cowboy.style.display = "none";
+    cowboy.classList.remove("a_cowboy3");
+    sang.classList.remove("a_sang3");
+};
+
+button4.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    locomotora.style.display = "block";
+    locomotora.classList.add("a_locomotora");
+    cowboy.style.display = "block";
+    cowboy.classList.add("a_cowboy4");
+    sang.classList.add("a_sang4");
+    setTimeout(rev_b4, 2300);
+});
+
+function rev_b4() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    locomotora.style.display = "none";
+    locomotora.classList.remove("a_locomotora");
+    cowboy.style.display = "none";
+    cowboy.classList.remove("a_cowboy4");
+    sang.classList.remove("a_sang4");
+};
+
+button5.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    tabac1.style.display = "block";
+    tabac1.classList.add("a_tabac1");
+    tabac2.style.display = "block";
+    tabac2.classList.add("a_tabac2");
+    tabac3.style.display = "block";
+    tabac3.classList.add("a_tabac3");
+    tabac4.style.display = "block";
+    tabac4.classList.add("a_tabac4");
+    tabac5.style.display = "block";
+    tabac5.classList.add("a_tabac5");
+    pot_tabac.style.display = "block";
+    pot_tabac.classList.add("a_pot_tabac");
+    setTimeout(rev_b5, 3500);
+});
+
+function rev_b5() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    tabac1.style.display = "none";
+    tabac1.classList.remove("a_tabac1");
+    tabac2.style.display = "none";
+    tabac2.classList.remove("a_tabac2");
+    tabac3.style.display = "none";
+    tabac3.classList.remove("a_tabac3");
+    tabac4.style.display = "none";
+    tabac4.classList.remove("a_tabac4");
+    tabac5.style.display = "none";
+    tabac5.classList.remove("a_tabac5");
+    pot_tabac.style.display = "none";
+    pot_tabac.classList.remove("a_pot_tabac");
+};
+
+button6.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    whisky_ple1.style.display = "block";
+    whisky_ple1.classList.add("a_whisky_ple1");
+    whisky_buit1.classList.add("a_whisky_buit1");
+    whisky_ple2.classList.add("a_whisky_ple2");
+    whisky_buit2.classList.add("a_whisky_buit2");
+    whisky_ple3.classList.add("a_whisky_ple3");
+    whisky_buit3.classList.add("a_whisky_buit3");
+    whisky_ple4.classList.add("a_whisky_ple4");
+    whisky_buit4.classList.add("a_whisky_buit4");
+    whisky_ple5.classList.add("a_whisky_ple5");
+    whisky_buit5.classList.add("a_whisky_buit5");
+    setTimeout(rev_b6, 4500);
+});
+
+function rev_b6() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    whisky_ple1.style.display = "none";
+    whisky_ple1.classList.remove("a_whisky_ple1");
+    whisky_buit1.classList.remove("a_whisky_buit1");
+    whisky_ple2.classList.remove("a_whisky_ple2");
+    whisky_buit2.classList.remove("a_whisky_buit2");
+    whisky_ple3.classList.remove("a_whisky_ple3");
+    whisky_buit3.classList.remove("a_whisky_buit3");
+    whisky_ple4.classList.remove("a_whisky_ple4");
+    whisky_buit4.classList.remove("a_whisky_buit4");
+    whisky_ple5.classList.remove("a_whisky_ple5");
+    whisky_buit5.classList.remove("a_whisky_buit5");
+};
+
+button7.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    bota_e.style.display = "block";
+    bota_e.classList.add("a_bota_e");
+    bota_d.style.display = "block";
+    bota_d.classList.add("a_bota_d");
+    setTimeout(rev_b7, 2000);
+});
+
+function rev_b7() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    bota_e.style.display = "none";
+    bota_e.classList.remove("a_bota_e");
+    bota_d.style.display = "none";
+    bota_d.classList.remove("a_bota_d");
+};
+
+button8.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    i_div.classList.add("red_div");
+    kill.style.display = "block";
+    kill.classList.add("a_kill");
+    skull1.style.display = "block";
+    skull1.classList.add("a_skull1");
+    skull2.style.display = "block";
+    skull2.classList.add("a_skull2");
+    skull3.style.display = "block";
+    skull3.classList.add("a_skull3");
+    skull4.style.display = "block";
+    skull4.classList.add("a_skull4");
+    skull5.style.display = "block";
+    skull5.classList.add("a_skull5");
+    skull6.style.display = "block";
+    skull6.classList.add("a_skull6");
+    setTimeout(rev_b8, 2500);
+});
+
+function rev_b8() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    i_div.classList.remove("red_div");
+    kill.style.display = "none";
+    kill.classList.remove("a_kill");
+    skull1.style.display = "none";
+    skull1.classList.remove("a_skull1");
+    skull2.style.display = "none";
+    skull2.classList.remove("a_skull2");
+    skull3.style.display = "none";
+    skull3.classList.remove("a_skull3");
+    skull4.style.display = "none";
+    skull4.classList.remove("a_skull4");
+    skull5.style.display = "none";
+    skull5.classList.remove("a_skull5");
+    skull6.style.display = "none";
+    skull6.classList.remove("a_skull6");
+};
+
+button9.addEventListener("click", function() {
+    i_div.style.display = "block";
+    i_div.classList.add("a_div");
+    i_div.classList.add("green_div");
+    levelup.style.display = "block";
+    levelup.classList.add("a_levelup");
+    fletxa1.style.display = "block";
+    fletxa1.classList.add("a_fletxa1");
+    fletxa2.style.display = "block";
+    fletxa2.classList.add("a_fletxa2");
+    fletxa3.style.display = "block";
+    fletxa3.classList.add("a_fletxa3");
+    fletxa4.style.display = "block";
+    fletxa4.classList.add("a_fletxa4");
+    fletxa5.style.display = "block";
+    fletxa5.classList.add("a_fletxa5");
+    fletxa6.style.display = "block";
+    fletxa6.classList.add("a_fletxa6");
+    setTimeout(rev_b9, 2500);
+});
+
+function rev_b9() {
+    i_div.style.display = "none";
+    i_div.classList.remove("a_div");
+    i_div.classList.remove("green_div");
+    levelup.style.display = "none";
+    levelup.classList.remove("a_levelup");
+    fletxa1.style.display = "none";
+    fletxa1.classList.remove("a_fletxa1");
+    fletxa2.style.display = "none";
+    fletxa2.classList.remove("a_fletxa2");
+    fletxa3.style.display = "none";
+    fletxa3.classList.remove("a_fletxa3");
+    fletxa4.style.display = "none";
+    fletxa4.classList.remove("a_fletxa4");
+    fletxa5.style.display = "none";
+    fletxa5.classList.remove("a_fletxa5");
+    fletxa6.style.display = "none";
+    fletxa6.classList.remove("a_fletxa6");
+};
