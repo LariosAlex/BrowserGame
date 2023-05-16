a61c771e9f3bbcb23b2c95773a6efc945d303ab1
Alex Larios <alexlarios49@gmail.com>
2023-05-16 12:43:42 +0200

    no action in console message

--- vue-bg/src/components/CharacterLog.vue
+++ vue-bg/src/components/CharacterLog.vue
@@ -7,7 +7,10 @@
     <div v-if="isModalOpen" class="fixed inset-x-0 bottom-20 flex items-center justify-end right-5 z-50 max-h-30">
       <div class="bg-black w-1/5 border-2 border-green-400 p-4 relative" style="height: 500px">
         <ol class="relative border-l border-gray-200 dark:border-gray-700 h-full overflow-auto flex-grow">
-          <li v-for="action in actions" :key="action" class="mb-10 ml-6">            
+          <li v-if="actions.length < 1" class="mb-10 ml-6">
+            <p class="mb-4 text-base font-normal text-green-400 dark:text-white">NO S'HA EFECTUAT CAP ACCIÓ ENCARA</p>
+          </li>
+          <li v-else v-for="action in actions" :key="action" class="mb-10 ml-6">            
             <h3 class="flex items-center text-center mb-1 text-lg font-semibold text-green-400 dark:text-white">{{ action.action__name }}</h3>
             <time class="block mb-2 text-sm font-normal leading-none text-green-400 dark:text-white">{{ formatDateTime(action.datetime) }}</time>
             <p class="mb-4 text-base font-normal text-green-400 dark:text-white">% EXIT: {{ action.action__success_rate }}  -- TIRADA: {{ action.run_number }}</p>
--- vue-bg/src/main.js
+++ vue-bg/src/main.js
@@ -4,7 +4,7 @@ import CharacterItems from './components/CharacterItems.vue'
 import CharacterActions from './components/CharacterActions.vue'
 import ActionButtons from './components/ActionsButtons.vue'
 import CharacterInfo from './components/CharacterInformation.vue'
-import CharacterLog from './components/CharacterLog'
+import CharacterLog from './components/CharacterLog.vue'
 
 createApp(RankingList).mount('#ranking')
 createApp(CharacterItems).mount('#infoCharacter')
