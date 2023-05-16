432308ea0ee491ae85f8687b0fbd5499cd51f19e
UnaiRiccoMoyano <un4i37@gmail.com>
2023-05-16 17:39:40 +0200

    prueba4

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
-    os.path.join(BASE_DIR, 'vue-bg/dist'),  # Bundle de VUE
+    os.path.join(BASE_DIR, 'vue-bg/.dist'),  # Bundle de VUE
 ]
 
 # Default primary key field type
