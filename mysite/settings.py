518a521e17bb0ee55559845ebe5fedf20f6719b0
UnaiRiccoMoyano <un4i37@gmail.com>
2023-05-16 18:11:56 +0200

    prueba1000

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,15 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-    #os.path.join(BASE_DIR, "vue-bg/dist"), # Bundle de VUE
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
